d36286b159c17a0246f902ad1fff5614330b11d6
Hetosk1 <hetekta2709@gmail.com>
2025-11-04 16:27:26 +0530

    Linear search

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ app.get('/', (request, response) => {
 
 })
 
+app.get('/linear', (request, response) => {
+	return response.json({
+		"code": `		
+def linear_search(arr, key):
+    for i in range(0,  len(arr)+1):
+        if arr[i] == key:
+            return i+1
+    return -1
+
+
+print(linear_search([1, 2, 3, 4, 5], 3))
+		`
+	})
+})
+
 app.listen(5000, () => {
 	console.log("chalu thai gyu");
 })
